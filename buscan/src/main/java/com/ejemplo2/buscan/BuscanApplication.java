7300d6903869b391de72391ae53f1510955c4e6b
SilviaBec <silvibecerra681@gmail.com>
2023-01-09 12:09:22 -0500

    Proyecto 1

---
+++ buscan/src/main/java/com/ejemplo2/buscan/BuscanApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo2.buscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscanApplication.class, args);
+	}
+
+}
---
+++ buscan/src/test/java/com/ejemplo2/buscan/BuscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo2.buscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
