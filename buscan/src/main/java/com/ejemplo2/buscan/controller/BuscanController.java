c6b2e2fefcc34135d0032812ce8f1d297e563adc
SilviaBec <silvibecerra681@gmail.com>
2023-01-14 21:25:02 -0500

    Controller updated

--- buscan/src/main/java/com/ejemplo2/buscan/controller/BuscanController.java
+++ buscan/src/main/java/com/ejemplo2/buscan/controller/BuscanController.java
@@ -1,5 +1,28 @@
 package com.ejemplo2.buscan.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class BuscanController {
+
+    @GetMapping("/servicios")
+    public String goToServicios(Model model){
+        model.addAttribute("name", "Bienvenid@ a mi pagina");
+        return "servicios";
+    }
+
+    @GetMapping("/contacto")
+    public String goToContacto(Model model){
+        return "contacto";
+    }
+
+    @GetMapping("/inicioSesion")
+    public String goToInicioSesion(Model model){
+        return "inicioSesion";
+    }
+    
     
 }
